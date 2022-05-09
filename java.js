68bd8579931c427378e6964c817b1d956dede8fa
rayy12 <105189094+rayy12@users.noreply.github.com>
2022-05-09 11:56:59 +0700

    FIRST UPLOAD

---
+++ java.js
@@ -0,0 +1,146 @@
+let gadget = [
+{
+        kode:"BRG001",
+        nama: "Iphone 13 Pro Max",
+        stock : "10",
+        rating: "5,0",
+        foto:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-max-green-select?wid=470&hei=556&fmt=png-alpha&.v=1644969385495",
+        harga:"22000000",
+},
+{
+kode:"BRG002",
+        nama:"Samsung A7 Lite",
+        stock : "20",
+        rating: "5,0",
+        foto:"http://p-id.ipricegroup.com/uploaded_6a60b762bd1ed9d7b15d6755dba3262f.jpg",
+        harga:"50000000",
+},
+{
+    kode:"BRG003",
+    nama:"Oppo Reno 9",
+        stock : "100",
+        rating: "5,0",
+        foto:"https://jasystore.com/wp-content/uploads/2021/12/oppo-reno8-5g-1-500x500.jpg",
+        harga:"70000000",
+},
+{
+
+    kode:"BRG004",
+    nama:"Xiaomi Redmi Note 11",
+        stock : "1000",
+        rating: "5,0",
+        foto:"https://i01.appmifile.com/webfile/globalimg/products/pc/redmi-note-11/ipgone003.png",
+        harga:"50000000",   
+},
+{
+
+    kode:"BRG005",
+    nama:"Vivo X70 Pro",
+        stock : "50",
+        rating: "5,0",
+        foto:"https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1633005750593/aeea29213c535d3af2af6aeba809a808.png",
+        harga:"60000000",   
+},
+{
+    kode:"BRG006",
+    nama: "ROG",
+    stock : "1220",
+    rating: "7,0",
+    foto:"https://asset.kompas.com/crops/xP45G8vpHLkw8BtBiJIV_bD1__o=/0x0:1200x800/750x500/data/photo/2019/11/29/5de111699d211.jpg",
+    harga:"21000000",
+},
+{
+kode:"BRG007",
+    nama:"Samsung",
+    stock : "2780",
+    rating: "8,0",
+    foto:"https://img.tek.id/img/content/2019/03/16/14063/samsung-resmi-luncurkan-jajaran-laptop-notebook-9-mereka-kG1X3YsyXG.JPG",
+    harga:"12000000",
+},
+{
+kode:"BRG008",
+nama:"Hp",
+    stock : "6580",
+    rating: "6,7",
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.jagatreview.com%2F2021%2F11%2Freview-hp-pavilion-14-ec0008au-laptop-tipis-yang-kencang-serba-bisa-dan-terjangkau%2F&psig=AOvVaw2CQRUIdDWj45WntZ1E9L9g&ust=1650343251882000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCNiW2vHlnPcCFQAAAAAdAAAAABAJ",
+    harga:"10000000",
+},
+{
+kode:"BRG009",
+nama:"Asus",
+    stock : "500",
+    rating: "7,0",
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fceklist.id%2F18945%2Flaptop-asus-terbaik%2F&psig=AOvVaw0f-FZAQfGj_4lu6IMBVROM&ust=1650343360982000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCNC22aHmnPcCFQAAAAAdAAAAABAD",
+    harga:"15000000",   
+},
+{
+kode:"BRG010",
+nama:"Vivo",
+    stock : "5090",
+    rating: "5,8",
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Finet.detik.com%2Fconsumer%2Fd-5193649%2Fvivobook-ultra-14-laptop-untuk-anak-muda-harga-rp-8-jutaan&psig=AOvVaw07g7doUNAsPoNHDqB4vz2-&ust=1650343457372000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCLichM_mnPcCFQAAAAAdAAAAABAD",
+    harga:"14000000",   
+},
+{
+    kode:"BRG011",
+nama:"Realme C21y",
+    stock : "2220",
+    rating: "6,8",
+    foto:"https://i0.wp.com/gizmologi.id/wp-content/uploads/2021/07/realme-C21Y-001-1.jpg",
+    harga:"11000000",   
+},
+{
+    kode:"BRG012",
+nama:"Realme 3",
+    stock : "220",
+    rating: "7,8",
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gsmarena.com%2Frealme_3-9558.php&psig=AOvVaw2tXL3Y86GbAUlvLzfFa3Iq&ust=1650344062153000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCMDprO_onPcCFQAAAAAdAAAAABAD",
+    harga:"4000000",   
+},
+{
+    kode:"BRG013",
+nama:"Realme 2",
+    stock : "2420",
+    rating: "6,8",
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gsmarena.com%2Frealme_2-9299.php&psig=AOvVaw2nJUtFQK9EHVtwi_U_O-O7&ust=1650344165879000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCMiYvqDpnPcCFQAAAAAdAAAAABAD",
+    harga:"2000000",   
+},
+{
+    kode:"BRG014",
+nama:"Realme 1",
+    stock : "2420",
+    rating: "6,8",
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.begawei.com%2Fharga%2Foppo-realme-1&psig=AOvVaw0qJk5Zi1bABGRVVyKl8auL&ust=1650344275059000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCIjUoNTpnPcCFQAAAAAdAAAAABAD",
+    harga:"2000000",   
+},
+{
+    kode:"BRG015",
+nama:"Vivo Y93",
+    stock : "2320",
+    rating: "8,8",
+    foto:"https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1561531770264/7cfb109e891fe657d4bd33805fe0d5ac.jpg",
+    harga:"3000000",   
+},
+{
+    kode:"BRG016",
+nama:"Redmi 9T",
+    stock : "25520",
+    rating: "9,8",
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gsmarena.com%2Fxiaomi_redmi_9t-10670.php&psig=AOvVaw1-PB1Uy2mNBk56EB3Gxzz0&ust=1650344498595000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCNie8L7qnPcCFQAAAAAdAAAAABAI",
+    harga:"3000000",   
+},
+];
+console.log(gadget);
+
+function carigadget(nama) {
+    for (inddex in gadget) {
+        if(gadget[index].nama==nama){
+        }
+    }
+    }
+
+    if(carigadget(prompt("Masukkan nama barang"))){
+        alert("ketemu");
+    } else {
+        alert("gak ketemu");
+    }
